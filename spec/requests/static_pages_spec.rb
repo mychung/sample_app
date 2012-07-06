0d5b2d9760941fa5a195b15d5c0744b965471be8
Mark Chung <mark.y.chung@gmail.com>
2012-07-05 19:17:01 -0700

    update the route variables

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "StaticPages" do
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit home_path
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit home_path
 			page.should have_selector('title',:text => "#{base_title}")
 		end		
 
 		it "should not have a custome page title" do
-			visit '/static_pages/home'
+			visit home_path
 			page.should_not have_selector('title',:text => "| Home")
 		end		
 	end
