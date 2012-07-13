3cc2392001b4c87864bd9ec5a7e32ed5f105ba4b
Mark Chung <mark.y.chung@gmail.com>
2012-07-12 17:46:43 -0700

    add minor chapt7 exercise modifications

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,7 @@ describe "UserPages" do
   			before { click_button submit }
   			it { should have_selector('title', text: 'Sign up') }
   			it { should have_content('error') }
+  			it { should have_content('can\'t be blank') }
   		end
   	end
 
@@ -49,7 +50,15 @@ describe "UserPages" do
 
         it "should create a user" do
         	expect { click_button submit }.to change(User, :count).by(1)
-      end
+      	end
+
+      	describe "after saving the user" do
+      		before { click_button submit }
+      		let(:user) { User.find_by_email('user@example.com') }
+
+      		it { should have_selector('title', text: user.name) }
+      		it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      	end
   	end
 
   	describe ""
