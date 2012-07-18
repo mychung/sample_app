99ba238221c4522905daac6fac91e10e2a54056c
Mark Chung <mark.y.chung@gmail.com>
2012-07-18 11:39:49 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:password]) 
 			# sign the user in and redirect to the user's show page
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else 
 			# create an error message and re-render the signin form.
 			flash.now[:error] = 'Invalid email/password combination' # not quite right!					
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,16 +15,5 @@ describe ApplicationHelper do
       full_title("").should_not =~ /\|/
     end
   end
-  
-  def valid_signin(user)
-    fill_in "Email",    with: user.email
-    fill_in "Password", with: user.password
-    click_button "Sign in"
-  end
 
-  RSpec::Matchers.define :have_error_message do |message|
-    match do |page|
-      page.should have_selector('div.alert.alert-error', text: message)
-    end
-  end
 end
